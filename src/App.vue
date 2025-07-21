4f5fb3e1371ef7bbee0125d3bbeba2289492a7a5
GhulamShabbir1 <ghulamshabir7869@gmail.com>
2025-07-21 11:36:00 +0500

    mothods and events

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <AppNavbar msg="this Add by me"/>
-    <Navbar/>
+    <AppEvent/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import AppNavbar from './components/AppNavbar.vue';
-
+import AppEvent from './components/AppEvent.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    AppNavbar
+    AppNavbar,
+    AppEvent
   }
 }
 </script>
---
+++ src/components/AppEvent.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <button v-on:click="HandleClick()">Click Me</button>
+</div>
+</template>
+
+<script>
+export default {
+    Name:"AppEvent",
+    methods:{
+        
+        HandleClick() {
+            alert("Rest in Peace")
+        }
+    }
+}
+
+</script>
